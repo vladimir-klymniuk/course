79ebd1e0324ca64d5c5f77d3d02635bcff7d056b
dk <dk@Users-MacBook-Pro.local>
2022-06-08 13:04:50 +0300

    Wed 8

---
+++ Основы программирования на языке С++/lesson_1/Source.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+// #include"windows.h"
+
+using namespace std;
+
+int main() {
+    // setlocale(LC_ALL, "rus");
+    cout << "\"Hello\" world\n";
+    cout << "\tHi";
+    cout << "\t\\ииііііырырыры\\";
+
+    return 0;
+}
